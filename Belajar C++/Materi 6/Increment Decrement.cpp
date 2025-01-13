69f24f40765a44e00cbc6da7536b411c729a991c
Rahmat Riyadi <rahmatriyadiambis@gmail.com>
2025-01-13 13:24:56 +0800

    Materi hari ke 6

---
+++ Belajar C++/Materi 6/Increment Decrement.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    //Increment
+    //Post Increment
+    x = 1;
+    cout<<x++<<endl;
+    cout<<x++<<endl;
+    cout<<x++<<endl;
+
+    cout<<"============================="<<endl;
+
+    //Pre Increment
+    y =12;
+    cout<<++y<<endl;
+    cout<<++y<<endl;
+    cout<<++y<<endl;
+
+    /*
+    Sederhananya di sini angka akan dijumlahkan dengan angka di variabel itu sendiri
+    */
+
+}
