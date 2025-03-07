84305f31b021033e1818871b0ac7b052334aac1e
Rahmat Riyadi <rahmatriyadiambis@gmail.com>
2025-03-07 09:39:33 +0800

    tambah

---
+++ c++ sertifikasi/07_deklarasi dan cin/deklarasi.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    a = 10000;
+    cout<<a<<endl;
+
+    int b;
+    cout<<"Masukkan nilai: "<<endl;
+    cin>>b;
+    cout<<"Nilai b adalah: "<<b<<endl;
+}
